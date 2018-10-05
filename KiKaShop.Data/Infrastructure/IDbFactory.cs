29025acc5fa556f198735f631d5d1c2ff4fbd814
KIMDESKTOP\KIMPC <kikashop8@gmail.com>
2018-10-05 14:35:54 +0700

    add entity framework code first for data

---
+++ KiKaShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace KiKaShop.Data.Infrastructure
+{
+    //Giao thuc khoi tao cac Object Entity gian tiep
+    public interface IDbFactory : IDisposable
+    {
+        KikaShopDbContext Init();
+    }
+}
---
+++ KiKaShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿namespace KiKaShop.Data.Infrastructure
+{
+    //Đẩy vào DataBase - Làm nhiệm vụ Transaction
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private KikaShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public KikaShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
