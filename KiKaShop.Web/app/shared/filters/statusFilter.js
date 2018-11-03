b435594bcd4166e25512175467998b02131b75ef
KIMDESKTOP\KIMPC <kikashop8@gmail.com>
2018-11-03 19:04:55 +0700

    add filter status and format data in list

---
+++ KiKaShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,8 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) return 'Kích hoạt';
+            else return 'Khóa';
+        }
+    });
+})(angular.module('kikashop.common'));
