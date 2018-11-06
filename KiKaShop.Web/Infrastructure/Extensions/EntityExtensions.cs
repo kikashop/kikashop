46e8c5eb45240e28ff4aa88d495c61d40f8ad7fd
KIMDESKTOP\KIMPC <kikashop8@gmail.com>
2018-11-06 16:30:06 +0700

    form list add edit product

--- KiKaShop.Web/Infrastructure/Extensions/EntityExtensions.cs
+++ KiKaShop.Web/Infrastructure/Extensions/EntityExtensions.cs
@@ -97,6 +97,7 @@ namespace KiKaShop.Web.Infrastructure.Extensions
             product.MetaKeyword = productVm.MetaKeyword;
             product.MetaDescription = productVm.MetaDescription;
             product.Status = productVm.Status;
+            product.Tags = productVm.Tags  ;
         }
     }
 }
