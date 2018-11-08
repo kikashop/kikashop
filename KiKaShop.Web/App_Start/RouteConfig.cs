3342ba3036b12d11c5105eea6dfd3c539c58cae0
KIMDESKTOP\KIMPC <kikashop8@gmail.com>
2018-11-08 22:41:01 +0700

    Routing

--- KiKaShop.Web/App_Start/RouteConfig.cs
+++ KiKaShop.Web/App_Start/RouteConfig.cs
@@ -11,12 +11,40 @@ namespace KiKaShop.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //This prevents from directly access to some high-security Path -Kim
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+       name: "Login",
+       url: "dang-nhap.html",
+       defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+       namespaces: new string[] { "KiKaShop.Web.Controllers" }
+            );
+            routes.MapRoute(
+         name: "About",
+         url: "gioi-thieu.html",
+         defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+         namespaces: new string[] { "KiKaShop.Web.Controllers" }
+              );
+            routes.MapRoute(
+          name: "Product Category",
+          url: "{alias}.pc-{id}.html",
+          defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+          namespaces: new string[] { "KiKaShop.Web.Controllers" }
+               );
+
+            routes.MapRoute(
+           name: "Product",
+           url: "{alias}.p-{id}.html",
+           defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
+           namespaces: new string[] { "KiKaShop.Web.Controllers" }
+                );
+            //The default is always at the bottom -Kim
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "KiKaShop.Web.Controllers" }
             );
         }
     }
